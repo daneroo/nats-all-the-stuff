880fe498eb574aab1e199d451a28ac045b54e351
Daniel Lauzon <daniel.lauzon@gmail.com>
2020-12-20 17:28:49 -0500

    Pass basic Lighthous accessibility html.lang and head.title

--- packages/site/pages/_app.tsx
+++ packages/site/pages/_app.tsx
@@ -1,14 +1,21 @@
-import { AppProps } from "next/app"
-import { ChakraProvider, CSSReset } from "@chakra-ui/react"
-
+import { AppProps } from 'next/app'
+import Head from 'next/Head'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
+      <Head>
+        <title>NATS.ws Demo</title>
+        <meta name='author' content='Daniel Lauzon'></meta>
+        <meta
+          name='description'
+          content='Next.js / Chakra UI / NATS Websocket Demo'
+        ></meta>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
-
 }
 
 export default App
---
+++ packages/site/pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, {
+  DocumentContext,
+  Html,
+  Main,
+  NextScript
+} from 'next/document'
+
+// This is just the default document
+// We instantiated to add the <html lang='en' /> property
+
+class MyDocument extends Document {
+  static async getInitialProps (ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return initialProps
+  }
+
+  render () {
+    return (
+      <Html lang='en'>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
