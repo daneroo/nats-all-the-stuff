3d8436bd5aa7feba3e2f77e255884bffce829ffd
Daniel Lauzon <daniel.lauzon@gmail.com>
2020-12-20 17:40:13 -0500

    Fixed las outstanding Lighthouse issue

--- packages/site/pages/_document.tsx
+++ packages/site/pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, {
   DocumentContext,
+  Head,
   Html,
   Main,
   NextScript
@@ -17,6 +18,7 @@ class MyDocument extends Document {
   render () {
     return (
       <Html lang='en'>
+        <Head />
         <body>
           <Main />
           <NextScript />
